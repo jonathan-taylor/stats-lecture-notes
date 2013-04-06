bfacc73883da2437a197e0040a0299ce88ddd732
Jonathan Taylor <jonathan.taylor@stanford.edu>
2013-04-06 16:21:36 -0700

    finished one parameter notes

--- website.py
+++ website.py
@@ -27,33 +27,9 @@ def make_web(clean=True):
         shutil.copy(texfile, 'exercises/tmp.tex')
         os.system(r'cd exercises; pdflatex "\nonstopmode\input{tmp.tex}"; mv tmp.pdf "%s"' % (os.path.basename(texfile)[:-3] + 'pdf'))
 
-    os.system('mkdir -p  www/restricted/notebooks; cp latex/header.tex www;')
-    os.system("cp notebooks/*ipynb notebooks/*pdf www/restricted/notebooks ; cd web; %s make html; cp -r _build/html/* ../www; " % cmd)
-
-def make_pdf(clean=True):
-    if clean:
-        cmd = 'make clean;'
-    else:
-        cmd = ''
-    os.system("cd web; %s make latex;" % cmd)
-
-def make_epub_book(clean=True):
-    if clean:
-        cmd = 'make clean;'
-    else:
-        cmd = ''
-    os.system("cd book; %s make epub;" % cmd)
-
-def make_pdf_book(clean=True, pdf=True):
-    if clean:
-        cmd = 'make clean;'
-    else:
-        cmd = ''
-    os.system("cd book;  %s make latex; " % cmd)
-    if pdf:
-        os.system("cd book/_build/latex; make all-pdf;")
+    os.system('mkdir -p  www/restricted/notebooks ; mkdir www/exercises ; cp latex/header.tex www;')
+    os.system("cp exercises/*pdf www/exercises; notebooks/*ipynb notebooks/*pdf www/restricted/notebooks ; cd web; %s make html; cp -r _build/html/* ../www; " % cmd)
 
 def deploy():
     os.system("rsync -avz www/* jtaylo@cardinal.stanford.edu:/afs/ir/class/stats306b/WWW")
-    #os.system('''ssh jtaylo@cardinal.stanford.edu "rm -fr /afs/ir/class/stats306b/WWW/guests/* ; cp -r /afs/ir/class/stats306b/WWW/* /afs/ir/class/stats306b/WWW/guests; rm -fr /afs/ir/class/stats306b/WWW/guests/restricted/.htaccess /afs/ir/class/stats306b/WWW/guests/restricted/.htstaff" ''')
-    #os.system("rm www/restricted/notes/epsdice.pdf; scp htaccess_stats306b jtaylo@cardinal.stanford.edu:/afs/ir/class/stats306b/WWW/guests/.htaccess")
+
