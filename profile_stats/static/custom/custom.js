d856c5884988fb980c3c2d6b8f09d483afe5a8b8
Jonathan Taylor <jonathan.taylor@stanford.edu>
2013-10-14 15:42:51 -0700

    added static files to profile -- exercise not working currently

---
+++ profile_stats/static/custom/custom.js
@@ -0,0 +1,30 @@
+$.getScript('/static/custom/js/slide_meta.js');
+$.getScript('/static/custom/js/exercise.js');
+
+MathJax.Hub.Config({
+                        TeX: {
+			   equationNumbers : { autoNumber: "AMS"},
+                           extensions: ["AMSmath.js", "AMSsymbols.js", "autobold.js"],
+                           },
+                tex2jax: {
+                    inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+                    displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
+                },
+            displayIndent: "10em",
+            displayAlign: 'center',
+                "HTML-CSS": {
+                    styles: {'.MathJax_Display': {"margin": 4}}
+                }
+});
+
+
+console.log('loading header');
+$.get('/static/custom/header.tex', function(data) {
+	var newdiv = $('<div id="mathjaxheader"/>');
+	console.log('Mathjax header load was performed.');
+	$('body').append(newdiv);
+	$('#mathjaxheader').text("$$ " + data + " $$");
+	MathJax.Hub.Queue(['Typeset', MathJax.Hub, 'mathjaxheader']);
+    });
+MathJax.Hub.Update();
+
