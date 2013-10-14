405a2d9cc61af463e0c1845bc0a47b36ae1c05c2
Jonathan Taylor <jonathan.taylor@stanford.edu>
2013-10-14 15:17:25 -0700

    using new nbconvert to make web

--- website.py
+++ website.py
@@ -4,6 +4,24 @@ import scripts.strip_exercises as SE
 reload(SE)
 extract_exercises = SE.extract_exercises
 
+templates = {'stats306b':'templates/stats306b_article.tplx',
+             'stats191':'templates/stats191_article.tplx',
+             }
+
+def build_nbook(nbook, 
+                course='stats306b'):
+    nbook_dir = os.path.abspath(os.path.split(nbook)[0])
+    tmp_dir = tempfile.mkdtemp()
+    shutil.copy2(templates[course], tmp_dir)
+    cmd = '''
+cd %s; 
+ipython nbconvert --to=latex --post PDF --template %s "%s"; 
+cp *.pdf %s ''' % (tmp_dir, 
+                   os.path.split(templates[course])[1],
+                   os.path.abspath(nbook), 
+                   nbook_dir)
+    os.system(cmd)
+
 def make_web(clean=True, build_pdf=True):
 
     if clean:
@@ -12,20 +30,12 @@ def make_web(clean=True, build_pdf=True):
         cmd = ''
     
     if build_pdf:
-        for nbook in glob.glob('notebooks/*ipynb'):
-            os.system('nbconvert.py --format=latex "%s" ' % nbook)
-            texfile = (os.path.splitext(nbook)[0] + '.tex')
-            shutil.copy(texfile, 'notebooks/tmp.tex')
-            os.system(r'cd notebooks; pdflatex "\nonstopmode\input{tmp.tex}"; mv tmp.pdf "%s"' % (os.path.basename(texfile)[:-3] + 'pdf'))
-
-            extract_exercises([nbook], odir='exercises')
+        
+        for nbook in glob.glob('notebooks/stats306b/*ipynb'):
+            build_nbook(nbook)
+            extract_exercises([nbook], odir='exercises/stats306b')
             ebook = nbook.replace('notebooks', 'exercises')
-            cmd = 'nbconvert.py --format=latex "%s" ' % ebook
-            os.system(cmd)
-
-            texfile = (os.path.splitext(ebook)[0] + '.tex')
-            shutil.copy(texfile, 'exercises/tmp.tex')
-            os.system(r'cd exercises; pdflatex "\nonstopmode\input{tmp.tex}"; mv tmp.pdf "%s"' % (os.path.basename(texfile)[:-3] + 'pdf'))
+            build_nbook(ebook)
 
     os.system('mkdir -p  www/restricted/notebooks ; mkdir www/exercises ; cp latex/header.tex www;')
     os.system("mkdir www/data; cp data/* www/data; cp exercises/*pdf www/exercises; cp notebooks/*ipynb notebooks/*pdf www/restricted/notebooks ; cd web; %s;  make html; cp -r _build/html/* ../www; ")
