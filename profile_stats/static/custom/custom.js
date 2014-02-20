c1b835b7a3c1cad86f3ca8b6f956d471ccf7fc09
Jonathan Taylor <jonathan.taylor@stanford.edu>
2014-02-20 12:42:15 -0800

    adding live slideshow to profile

--- profile_stats/static/custom/custom.js
+++ profile_stats/static/custom/custom.js
@@ -1,3 +1,7 @@
+// we want strict javascript that fails
+// on ambiguous syntax
+"using strict";
+
 console.log('loading slide meta');
 $.getScript('/static/js/slide_meta.js');
 console.log('loading exercise');
@@ -28,3 +32,36 @@ $.get('/static/tex/header.tex', function(data) {
     MathJax.Hub.Queue(['Typeset', MathJax.Hub, 'mathjaxheader']);
 });
 MathJax.Hub.Update();
+
+
+// do not use notebook loaded  event as it is re-triggerd on
+// revert to checkpoint but this allow extesnsion to be loaded
+// late enough to work.
+//
+
+$([IPython.events]).on('app_initialized.NotebookApp', function(){
+
+
+	/**  Use path to js file relative to /static/ dir without leading slash, or
+	 *  js extension.
+	 *  Link directly to file is js extension.
+	 *
+	 *  first argument of require is a **list** that can contains several modules if needed.
+	 **/
+
+	// require(['custom/noscroll']);
+	// require(['custom/clean_start'])
+	// require(['custom/toggle_all_line_number'])
+	// require(['custom/gist_it']);
+
+	/**
+	 *  Link to entrypoint if extesnsion is a folder.
+	 *  to be consistent with commonjs module, the entrypoint is main.js
+	 *  here youcan also trigger a custom function on load that will do extra
+	 *  action with the module if needed
+	 **/
+	require(['custom/slidemode/main'],function(slidemode){
+		//     // do stuff
+	    })
+
+});
