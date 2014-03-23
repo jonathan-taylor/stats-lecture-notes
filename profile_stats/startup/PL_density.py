ad98f3e12ae723bd2516d6d98908575125022f7d
Jonathan Taylor <jonathan.taylor@stanford.edu>
2014-03-23 12:06:58 -0700

    working on histogram figures -- going to switch to scipy.kde and use xkcd

--- profile_stats/startup/PL_density.py
+++ profile_stats/startup/PL_density.py
@@ -87,3 +87,76 @@ def PL_density(mass, binpoints, facecolor='red',
 
    return ax
 
+def PL_add_patch(left, right, top, bottom, 
+                 facecolor='red',
+                 alpha=0.8, 
+                 edgecolor='gray', 
+                 ax=None,
+                 **opts):
+   """
+   Add one rectangular patch.
+
+   Parameters
+   ----------
+
+   left, right, top, bottom : `float`
+
+   facecolor : 
+       Color for each bar.
+
+   alpha : float
+       Opacity for each bar.
+
+   edgecolor : str
+       Color for each bar.
+
+   ax : `Axes`
+       Optional axes to add the density estimate to.
+
+   opts : {}
+       Additional arguments passed to `patches.PathPatch`
+
+
+   """
+
+   if ax is None:
+      fig = pylab.gcf()
+      ax = fig.add_subplot(111)
+
+   opts['facecolor'] = facecolor
+   opts['alpha'] = alpha
+   opts['edgecolor'] = edgecolor
+
+   XY = np.array([[left, left, right, right], 
+                  [bottom, top, top, bottom]]).reshape((2,4,1)).T
+   barpath = path.Path.make_compound_path_from_polys(XY)
+   
+   patch = patches.PathPatch(barpath, **opts)
+
+   ax.add_patch(patch)
+
+   # update the view limits
+
+   return ax
+
+def stylized_density(sample, ax=None):
+   IP = get_ipython()
+   IP.user_ns['sample'] = sample
+   IP.run_cell_magic('R', ' -o DY,DX -i sample', """
+DY = density(sample)$y
+DX = density(sample)$x
+""")
+
+   DX, DY = IP.user_ns['DX'], IP.user_ns['DY']
+   if ax is None:
+      fig = pylab.gcf()
+      ax = fig.add_subplot(111)
+
+   ax.fill_between(DX,DY,0*DX, facecolor='gray', alpha=0.7)
+   ax.plot(DX, DY, linewidth=4, c='k')
+   ax.set_ylabel('Density (% per unit change)', fontsize=20)
+   ax.set_xlabel('Units', fontsize=20)
+   ax.set_xticks([])
+   ax.set_yticks([])
+   del(IP.user_ns['DY']); del(IP.user_ns['DX'])
+   return ax
