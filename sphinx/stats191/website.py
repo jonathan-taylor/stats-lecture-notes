200710ae1f6752944a07455b98a4463bfe2bbaa8
Jonathan Taylor <jonathan.taylor@stanford.edu>
2014-03-24 23:23:36 -0700

    reworking make_web to not rebuild if no change

--- sphinx/stats191/website.py
+++ sphinx/stats191/website.py
@@ -1,23 +1,22 @@
-import os, glob, shutil
+import os, glob, shutil, filecmp
 
-def build_nbook(nbook, build_pdf=True):
+def build_nbook(nbook):
     nbook_dir = os.path.abspath(os.path.split(nbook)[0])
     shutil.copy2('stats191_article.tplx', nbook_dir)
     shutil.copy2('stats191_slides.tplx', nbook_dir)
     cmd = '''
-cd %s; 
-ipython nbconvert --to=slides "%s" --template=./stats191_slides.tplx;
-ipython nbconvert --to=html "%s";
- ''' % (nbook_dir, 
-        os.path.abspath(nbook),
-        os.path.abspath(nbook))
-    if build_pdf:
-        cmd += '''
+    cd %s; 
+    ipython nbconvert --to=slides "%s" --template=./stats191_slides.tplx;
+    ipython nbconvert --to=html "%s";
+    ''' % (nbook_dir, 
+            os.path.abspath(nbook),
+            os.path.abspath(nbook))
+    cmd += '''
 ipython nbconvert --to=latex --post PDF --template=stats191_article.tplx "%s";
 ''' % os.path.abspath(nbook)
     os.system(cmd)
 
-def make_web(clean=True, build_pdf=True):
+def make_web(clean=True, force=False):
 
     if clean:
         os.system('make clean;')
@@ -25,9 +24,20 @@ def make_web(clean=True, build_pdf=True):
     os.system('mkdir -p notebooks; cp -r ../../notebooks/stats191/* notebooks/')        
     os.system('mkdir -p www/R; cp R/* www/R; cp -r assignments/* notebooks/; rm -fr notebooks/oldslides notebooks/oldnotebooks')        
     os.system('cp -r assignments/* notebooks/; rm -fr notebooks/oldslides notebooks/oldnotebooks')        
-    if build_pdf:
-        for nbook in glob.glob('notebooks/*ipynb'):
-            build_nbook(nbook, build_pdf=build_pdf)
+
+    for nbook in glob.glob('notebooks/*ipynb'):
+        htmlfile = os.path.splitext(nbook)[0] + '.html'
+        pdffile = os.path.splitext(nbook)[0] + '.pdf'
+        try:
+            diff_file = not (filecmp.cmp(htmlfile, 
+                                         os.path.join('www', htmlfile))
+                             and filecmp.cmp(pdffile, 
+                                             os.path.join('www', pdffile)))
+        except OSError:
+            diff_file = True
+            
+        if force or diff_file:
+            build_nbook(nbook)
 
     os.system('mkdir -p  www/notebooks/figs ; cp latex/header.tex www;')
     os.system("mkdir -p www/data; cp ../../data/* www/data; cp -r notebooks/* www/notebooks; make html; cp -r _build/html/* www; ")
