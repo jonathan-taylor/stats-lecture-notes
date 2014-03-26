1b4602419ed72355580c8ed4643b0796c884e4d5
Jonathan Taylor <jonathan.taylor@stanford.edu>
2014-03-26 01:23:50 -0700

    updated building webpage, added stats60 sphinx for building web

--- sphinx/stats60/website.py
+++ sphinx/stats60/website.py
@@ -1,51 +1,64 @@
 import os, glob, shutil, filecmp
 
-def build_nbook(nbook):
+def build_nbook(nbook, strip=True):
     nbook_dir = os.path.abspath(os.path.split(nbook)[0])
     shutil.copy2('stats60_article.tplx', nbook_dir)
     shutil.copy2('stats60_slides.tplx', nbook_dir)
-    cmd = '''
+    shutil.copy2('stats60_html.tplx', nbook_dir)
+    if strip:
+        cmd = '''
+    python ../../tools/strip_skip.py "%s"
+        ''' % os.path.abspath(nbook)
+    else:
+        cmd = ''
+    nbook = nbook.replace(".ipynb", "_stripped.ipynb")
+    cmd += '''
     cd %s; 
     ipython nbconvert --to=slides "%s" --template=./stats60_slides.tplx;
-    ipython nbconvert --to=html "%s";
+    ipython nbconvert --to=html "%s" --template=./stats60_html.tplx;
+    ipython nbconvert --to=latex --post PDF --template=stats60_article.tplx "%s";
     ''' % (nbook_dir, 
             os.path.abspath(nbook),
+            os.path.abspath(nbook),
             os.path.abspath(nbook))
-    cmd += '''
-ipython nbconvert --to=latex --post PDF --template=stats60_article.tplx "%s";
-''' % os.path.abspath(nbook)
     os.system(cmd)
 
-def make_web(clean=True, force=False):
+def make_web(clean=True, force=False, strip=True):
 
     if clean:
         os.system('make clean;')
     
-    os.system('mkdir -p notebooks; cp -r ../../notebooks/stats60/index.ipynb .')        
-    os.system('ipython nbconvert --to rst index.ipynb; rm index.ipynb')
-    os.system('mkdir -p notebooks; cp -r ../../notebooks/stats60/* notebooks/')        
-    os.system('mkdir -p www/R; cp R/* www/R; cp -r assignments/* notebooks/; rm -fr notebooks/oldslides notebooks/oldnotebooks')        
-    os.system('cp -r assignments/* notebooks/; rm -fr notebooks/oldslides notebooks/oldnotebooks')        
+    for dir in ['www', 'notebooks']:
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+    os.system('''
+    cp -r ../../notebooks/stats60/index.ipynb .;
+    ipython nbconvert --to rst index.ipynb; rm index.ipynb ;
+    ''')
 
-    for nbook in glob.glob('notebooks/*ipynb'):
-        htmlfile = os.path.splitext(nbook)[0] + '.html'
-        pdffile = os.path.splitext(nbook)[0] + '.pdf'
+    for obook in glob.glob('../../notebooks/stats60/*ipynb'):
+        nbook = obook.replace('../../', './').replace('stats60/', '')
         try:
-            diff_file = not (filecmp.cmp(htmlfile, 
-                                         os.path.join('www', htmlfile))
-                             and filecmp.cmp(pdffile, 
-                                             os.path.join('www', pdffile)))
+            diff = not filecmp.cmp(obook, nbook)
         except OSError:
-            diff_file = True
-            
-        if force or diff_file:
-            build_nbook(nbook)
+            diff = True
+        if diff:
+            shutil.copy(obook, nbook) 
+            build_nbook(nbook, strip=strip)
 
-    os.system('mkdir -p  www/notebooks/figs ; cp latex/header.tex www;')
-    os.system("mkdir -p www/data; cp ../../data/* www/data; cp -r notebooks/* www/notebooks; make html; cp -r _build/html/* www; ")
+    os.system('''
+    cp -r notebooks/*stripped* www/notebooks; 
+    make html; 
+    cp -r _build/html/* www; 
+    ''')
+
+    for f in glob.glob('www/notebooks/*stripped.*'):
+        os.rename(f, f.replace('_stripped', ''))
 
 def deploy():
-    os.system('rm -fr www/notebooks/oldnotebooks www/notebooks/oldslides ')
-    os.system("rsync -avz www/* jtaylo@corn.stanford.edu:/afs/ir/class/stats60/WWW/")
-    os.system("rsync -avz www/* jtaylo@miller.stanford.edu:stats-lecture-notes-working/sphinx/stats60/www")
+    os.system("""
+    rm -fr www/notebooks/oldnotebooks www/notebooks/oldslides ;
+    rsync -avz www/* jtaylo@corn.stanford.edu:/afs/ir/class/stats60/WWW/ ;)
+    rsync -avz www/* jtaylo@miller.stanford.edu:stats-lecture-notes-working/sphinx/stats60/www ;
+    """)
 
