1d5a69f0d64b57658f0fb889cf5448d065a21937
Jonathan Taylor <jonathan.taylor@stanford.edu>
2014-04-04 10:11:15 -0700

    added a new normal table

--- stats_lectures/stats60/tables.py
+++ stats_lectures/stats60/tables.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.stats import norm as ndist
 from ipy_table import make_table
 
-def table_a104(z):
+def symmetric_normal_table(z):
     """
     Produce a row of Table A104
     """
@@ -10,5 +10,13 @@ def table_a104(z):
         raise ValueError('z must be nonnegative')
     table = [('$z$', 'Height', 'Area'),
              (z, 100*ndist.pdf(z), 100*2*(ndist.cdf(z)-0.5))]
+    return make_table(table)
+
+def tail_normal_table(z):
+    """
+    Produce a row of Table A104
+    """
+    table = [('$z$', 'Height', 'Area'),
+             (z, 100*ndist.pdf(z), 100*ndist.cdf(z))]
     return make_table(table)
 
