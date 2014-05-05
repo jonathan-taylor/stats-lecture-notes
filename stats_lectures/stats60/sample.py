51fd9306c8bdcb3948f53dadb039621c778d179e
Jonathan Taylor <jonathan.taylor@stanford.edu>
2014-05-04 18:37:00 -0700

    color for sampling

--- stats_lectures/stats60/sample.py
+++ stats_lectures/stats60/sample.py
@@ -12,8 +12,6 @@ class Sample(ProbabilitySpace):
     alpha = 0.3
     ptsize = 5
     sample_ptsize = 60
-    colors = {'R':'red',
-              'B':'blue'}
 
     def __init__(self, nblue, nred, ndraw, draw_fig=True):
         self.nblue, self.nred, self.ndraw = nblue, nred, ndraw
@@ -23,11 +21,11 @@ class Sample(ProbabilitySpace):
         if draw_fig:
             self.draw()
 
-    def draw(self):
+    def draw(self, color={'R':'red','B':'blue'}):
         self.figure.clf()
         ax, X = self.axes, self.X
-        ax.scatter(X[:self.nblue,0], X[:self.nblue,1], s=self.ptsize, color=self.color['B'], alpha=self.alpha)            
-        ax.scatter(X[self.nblue:,0], X[self.nblue:,1], s=self.ptsize, color=self.color['R'], alpha=self.alpha)            
+        ax.scatter(X[:self.nblue,0], X[:self.nblue,1], s=self.ptsize, color=color['B'], alpha=self.alpha)            
+        ax.scatter(X[self.nblue:,0], X[self.nblue:,1], s=self.ptsize, color=color['R'], alpha=self.alpha)            
         ax.set_xticks([]);    ax.set_xlim([-0.01,1.01])            
         ax.set_yticks([]);    ax.set_ylim([-0.01,1.01])       
 
@@ -43,19 +41,21 @@ class Sample(ProbabilitySpace):
         self.figure
         return self._axes
 
-    def draw_sample_pts(self):
-        self.draw()
+    def draw_sample_pts(self, bgcolor={'R':'red','B':'blue'},
+                        color={'R':'red','B':'blue'}):
+        self.draw(color=bgcolor)
         ax, X = self.axes, self.X
         sample = self.box.sample(self.ndraw)
         nblue = np.sum([s == 'B' for s in sample])
         nred = self.ndraw - nblue
-        ax.scatter(X[:nblue,0], X[:nblue,1], s=self.sample_ptsize, color=self.color['B'], alpha=.8, edgecolor='gray', linewidth=2)            
-        ax.scatter(X[-nred:,0], X[-nred:,1], s=self.sample_ptsize, color=self.color['R'], alpha=.8, edgecolor='gray', linewidth=2)           
+        ax.scatter(X[:nblue,0], X[:nblue,1], s=self.sample_ptsize, color=color['B'], alpha=.8, edgecolor='gray', linewidth=2)            
+        ax.scatter(X[-nred:,0], X[-nred:,1], s=self.sample_ptsize, color=color['R'], alpha=.8, edgecolor='gray', linewidth=2)           
         return self.figure
 
-    def trial(self):
+    def trial(self, bgcolor={'R':'red','B':'blue'},
+              color={'R':'red','B':'blue'}):
         sample = self.box.sample(self.ndraw)
         if self.draw_fig:
-            self.draw_sample_pts()
+            self.draw_sample_pts(color=color, bgcolor=bgcolor)
         self.outcome = np.mean([s == 'B' for s in sample])
         return self.outcome
