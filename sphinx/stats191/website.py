b3f3095fab7841450ecd85c3edfa3d2849c2fcc3
Jonathan Taylor <jonathan.taylor@stanford.edu>
2015-01-08 11:29:36 -0800

    building assignments and removing unwanted ones

--- sphinx/stats191/website.py
+++ sphinx/stats191/website.py
@@ -1,12 +1,13 @@
 import os, glob, shutil, filecmp
+from stats_lectures.nbtools import strip_skipped_cells, reads, writes
 
 def build_nbook(nbook):
     nbook_dir = os.path.abspath(os.path.split(nbook)[0])
     shutil.copy2('stats191_article.tplx', nbook_dir)
     shutil.copy2('stats191_slides.tplx', nbook_dir)
     cmd = '''
-    cd %s; 
-    ipython nbconvert --to=slides "%s" --template=./stats191_slides.tplx;
+    cd "%s"; 
+    ipython nbconvert --to=slides "%s" --template=./stats191_slides.tplx --reveal-prefix=http://statweb.stanford.edu/~jtaylo/reveal.js;
     ipython nbconvert --to=html "%s";
     ''' % (nbook_dir, 
             os.path.abspath(nbook),
@@ -16,34 +17,73 @@ ipython nbconvert --to=latex --post PDF --template=stats191_article.tplx "%s";
 ''' % os.path.abspath(nbook)
     os.system(cmd)
 
-def make_web(clean=True, force=False):
+def make_web(clean=True, force=False, assigned=[1], solved=[]):
 
     if clean:
         os.system('make clean;')
     
-    os.system('mkdir -p notebooks; cp -r ../../notebooks/stats191/* notebooks/')        
-    os.system('mkdir -p www/R; cp R/* www/R; cp -r assignments/* notebooks/; rm -fr notebooks/oldslides notebooks/oldnotebooks')        
-    os.system('cp -r assignments/* notebooks/; rm -fr notebooks/oldslides notebooks/oldnotebooks')        
+    for dir in ['www', 'notebooks']:
+        if not os.path.exists(dir):
+            os.makedirs(dir)
 
-    for nbook in glob.glob('notebooks/*ipynb'):
-        htmlfile = os.path.splitext(nbook)[0] + '.html'
-        pdffile = os.path.splitext(nbook)[0] + '.pdf'
+    for obook in glob.glob('../../notebooks/stats191/*ipynb'):
+        nbook = obook.replace('../../', './').replace('stats191/', '')
+        print nbook
+        if not os.path.exists(os.path.dirname(nbook)):
+            os.makedirs(os.path.dirname(nbook))
         try:
-            diff_file = not (filecmp.cmp(htmlfile, 
-                                         os.path.join('www', htmlfile))
-                             and filecmp.cmp(pdffile, 
-                                             os.path.join('www', pdffile)))
+            diff = not filecmp.cmp(obook, nbook)
         except OSError:
-            diff_file = True
-            
-        if force or diff_file:
-            build_nbook(nbook)
+            diff = True
 
-    os.system('mkdir -p  www/notebooks/figs ; cp latex/header.tex www;')
-    os.system("mkdir -p www/data; cp ../../data/* www/data; cp -r notebooks/* www/notebooks; make html; cp -r _build/html/* www; ")
+        if diff:
+            shutil.copy(obook, nbook) 
+            with open(nbook, 'r') as f:
+                nb = reads(f.read(), 'json')
+            print 'running and stripping skipped cells from notebook %s' % nbook
+            stripped_nb = strip_skipped_cells(nb, timeout=10)
+
+            new_nbook = nbook.replace('notebooks', 'built_notebooks')
+            if not os.path.exists(os.path.dirname(new_nbook)):
+                os.makedirs(os.path.dirname(new_nbook))
+            with open(new_nbook, 'w') as f:
+                f.write(writes(nb, 'json'))
+            build_nbook(new_nbook)
+
+    for dirname in ['built_notebooks']:
+        wwwdir = dirname.replace('built_notebooks', 'www/notebooks')
+        if not os.path.exists(wwwdir):
+            shutil.copytree(dirname, wwwdir)
+        else:
+            for f in glob.glob('%s/*' % dirname):
+                if not os.path.isdir(f):
+                    shutil.copy2(f, wwwdir)
+
+    for f in (glob.glob('www/notebooks/*stripped.*') + 
+              glob.glob('www/notebooks/*stripped.*')):
+        os.rename(f, f.replace('_stripped', ''))
+
+    del_assign = set(range(1,6)).difference(assigned)
+    for assign in del_assign:
+        for f in glob.glob('www/notebooks/Assignment%d*' % assign):
+            os.remove(f)
+
+    del_solved = set(range(1,6)).difference(solved)
+    for solve in del_solved:
+        for f in glob.glob('www/notebooks/Solution%d*' % solve):
+            os.remove(f)
+
+    cmd = '''
+    make html; 
+    rm -fr _build/html/quizzes;
+    rm -fr _build/html/_sources;
+    cp -r _build/html/* www; 
+    '''
+    os.system(cmd)
+
 
 def deploy():
     os.system('rm -fr www/notebooks/oldnotebooks www/notebooks/oldslides ')
     os.system("rsync -avz www/* jtaylo@corn.stanford.edu:/afs/ir/class/stats191/WWW/")
-    os.system("rsync -avz www/* jtaylo@miller.stanford.edu:stats-lecture-notes-working/sphinx/stats191/www")
+#    os.system("rsync -avz www/* jtaylo@miller.stanford.edu:stats-lecture-notes-working/sphinx/stats191/www")
 
